cf9edb49e1c264ec6513727dbb27ac0eac1619b7
Piyush762051 <khandarepiyush2@gmail.com>
2024-11-12 13:00:26 +0530

    Update GitHome.java

--- GitDemoProjcet/src/com/cjc/model/GitHome.java
+++ GitDemoProjcet/src/com/cjc/model/GitHome.java
@@ -3,9 +3,13 @@ package com.cjc.model;
 public class GitHome 
 {
 	public void m1()
-	
 	{
 		System.out.println("this is M1 method");
+	}
+
+	public void m2()
+	{
+		System.out.println("this is M2 method");
 	}
    
 	public static void main(String[] args) 
